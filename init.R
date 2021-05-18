48680308a5a398cb33e28f7d7f1df2a2f9b228ae
Duc-Quang Nguyen <ducquang.nguyen@gmail.com>
2021-05-18 17:09:38 +0200

    try other wa to install magick on heroku via install_github

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shiny", "colourpicker", "shinythemes", "thematic",
+my_packages = c("remotes", "shiny", "colourpicker", "shinythemes", "thematic",
                 "htmltools", "dplyr", "tidyr", "magick", "ggforce")
 
 install_if_missing = function(p) {
@@ -12,4 +12,16 @@ install_if_missing = function(p) {
   }
 }
 
+system("Package Magick++ was not found in the pkg-config search path.
+       Perhaps you should add the directory containing `Magick++.pc'
+       to the PKG_CONFIG_PATH environment variable
+       No package 'Magick++' found")
 invisible(sapply(my_packages, install_if_missing))
+
+
+if ("magick" %in% rownames(installed.packages()) == FALSE) {
+  remotes::install_github("ropensci/magick")
+}
+
+
+
